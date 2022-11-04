441e5722ba80e96a8195cb7f6d474964ff593339
[chirazbdh] <[boudhriouachiraz1@gmail.com]>
2022-11-04 10:45:17 +0100

    'testcommit'

--- server.js
+++ server.js
@@ -55,11 +55,14 @@ var year = date_ob.getFullYear();
 var date = year + "-" + month + "-" + day;
 console.log(date);
     
-var hours = date_ob.getHours();
+//var hours = date_ob.getHours();
 var minutes = date_ob.getMinutes();
 var seconds = date_ob.getSeconds();
-
-var dateTime = year + "-" + month + "-" + day + " " + hours + ":" + minutes;
+  if(cnt>=60){
+	  h=h+1;
+	  cnt=0;
+  }
+var dateTime = year + "-" + month + "-" + day + " " + h.toString() + ":" + minutes;
 console.log(dateTime);
         var messageObject = {
             //topic: topic,
@@ -67,6 +70,7 @@ console.log(dateTime);
 			date: dateTime
         };
    collection.insert(messageObject, function(error, result) {
+cnt=cnt+1;
   collection.find({}).toArray(function(err, data){
       console.log(data); // it will print your collection data
   resultt=data;
